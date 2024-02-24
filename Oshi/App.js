5d1cb15077e7af3c04673ff04e368ba81384b9fd
WhereIsMyPenguin <86001892+WhereIsMyPenguin@users.noreply.github.com>
2024-02-24 11:51:13 +0900

    Starts Here

---
+++ Oshi/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header">
+        <header>
+          <h1>夏色まつり</h1>
+        </header>
+        
+        <div class="contents-item">
+            <img src="https://hololive.hololivepro.com/wp-content/uploads/2020/06/matsuri_pr-img_2-768x1152.png" />
+            <p>まつりちゃん(通常衣装)</p>
+          </div>
+          <div class="contents-item">
+            <img src="https://hololive.hololivepro.com/wp-content/uploads/2020/06/Natsuiro-Matsuri_pr-img_08.png" />
+            <p>まつりちゃん(大人衣装)</p>
+          </div>
+        <a
+          className="App-link"
+          href="https://hololive.hololivepro.com/talents/natsuiro-matsuri/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          </a>
+          <div class="名前">
+          <h1>
+            夏色まつり
+          </h1>
+          <h5>
+            ホロライブ1期生の自称清楚担当。<br/>
+            チア部の新入生。<br/>
+            上記で書いたように清楚担当であると<br/>
+            本人は自称しているが<br/>
+            ホロライブプロダクションに所属する<br/>
+            バーチャルYouTuberから「夏色よりひどいことをしなければ大丈夫」と評され、ボーダーラインとして<br/>
+            
+            <k>『まつりライン』</k>というものが存在する。
+             </h5>
+             
+      </div>
+       <a href="https://www.youtube.com/@NatsuiroMatsuri">youtube</a>
+      <span class="blank_space"></span>
+       <a href="https://twitter.com/natsuiromatsuri">X</a>
+      </header>
+      <div class="header">
+      <div>
+      <h4 class="誕生日">誕生日<span class="blank_space"></span>7月22日</h4>
+        <h4 class="イラストレーター">イラストレーター<span class="blank_space"></span>皆村春希</h4>
+        <h4 class="ファンネーム">ファンネーム<span class="blank_space"></span>まつりす</h4> 
+        <h4 class="ファンアート">ファンアートタグ<span class="blank_space"></span>祭絵</h4>
+       
+        </div>
+        <footer>
+        <a href="https://shop.hololivepro.com/pages/search-results-page?q=%E5%A4%8F%E8%89%B2%E3%81%BE%E3%81%A4%E3%82%8A"><h2>公式ショップ</h2></a>
+        </footer>
+    
+    </div>
+
+    </div>
+  );
+}
+
+
+export default App;
